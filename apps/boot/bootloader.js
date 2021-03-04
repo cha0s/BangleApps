d3b37b2fdf466fd931643f24a4385a3cc825ab3a
cha0s <git@cha0s.io>
2021-03-04 04:23:24 -0600

    refactor: real random clock support

--- apps/boot/bootloader.js
+++ apps/boot/bootloader.js
@@ -1,25 +1,48 @@
-// This runs after a 'fresh' boot
-var clockApp=(require("Storage").readJSON("setting.json",1)||{}).clock;
-if (clockApp) clockApp = require("Storage").read(clockApp);
-if (!clockApp) {
-  clockApp = require("Storage").list(/\.info$/)
-    .map(file => {
-      const app = require("Storage").readJSON(file,1);
-      if (app && app.type == "clock") {
-        return app;
-      }
-    })
-    .filter(x=>x)
-    .sort((a, b) => a.sortorder - b.sortorder)[0];
-  if (clockApp)
-    clockApp = require("Storage").read(clockApp.src);
-}
-if (!clockApp) clockApp=`E.showMessage("No Clock Found");
-setWatch(() => {
-  Bangle.showLauncher();
-}, BTN2, {repeat:false,edge:"falling"});)
-`;
-// check to see if our clock is wrong - if it is use GPS time
+// This runs after a 'fresh' boot. Everything allocated in this file is
+// persistent by default, so be mindful!
+const holder = {};
+holder.evaluateCurrentClockApp = () => {
+  const allClockApps = () => (
+    require("Storage").list(/\.info$/)
+      .map((file) => {
+        const app = require("Storage").readJSON(file,1);
+        if (app && app.type == "clock") {
+          return app;
+        }
+      })
+      .filter((x) => x)
+  );
+  const clockNotFound = `
+    E.showMessage("No Clock Found");
+    setWatch(() => {
+      Bangle.showLauncher();
+    }, BTN2, {repeat:false,edge:"falling"});)
+  `;
+  const currentClockApp = () => {
+    const {clock: clockApp} = require("Storage").readJSON("setting.json",1) || {};
+    if (" random" === clockApp) {
+      const apps = allClockApps();
+      return apps.length > 0
+        ? require("Storage").read(apps[Math.floor(Math.random() * apps.length)].src)
+        : clockNotFound;
+    } else if (clockApp) {
+      return require("Storage").read(clockApp);
+    }
+    // Fallback: first clock app we discover on the system
+    const [discoveredApp] = allClockApps().sort((a, b) => a.sortorder - b.sortorder);
+    return discoveredApp
+      ? require("Storage").read(discoveredApp.src)
+      // Fallback: not found
+      : `E.showMessage("No Clock Found");
+        setWatch(() => {
+          Bangle.showLauncher();
+        }, BTN2, {repeat:false,edge:"falling"});)
+      `;
+  };
+  delete holder.evaluateCurrentClockApp;
+  eval(currentClockApp());
+};
+// Check to see if our clock is wrong - if it is use GPS time
 if ((new Date()).getFullYear()<2000) {
   E.showMessage("Searching for\nGPS time");
   Bangle.on("GPS",function cb(g) {
@@ -28,8 +51,7 @@ if ((new Date()).getFullYear()<2000) {
     if (!g.time || (g.time.getFullYear()<2000) ||
        (g.time.getFullYear()>2200)) {
       // GPS receiver's time not set - just boot clock anyway
-      eval(clockApp);
-      delete clockApp;
+      holder.evaluateCurrentClockApp();
       return;
     }
     // We have a GPS time. Set time and reboot (to load alarms properly)
@@ -38,6 +60,5 @@ if ((new Date()).getFullYear()<2000) {
   });
   Bangle.setGPSPower(1);
 } else {
-  eval(clockApp);
-  delete clockApp;
+  holder.evaluateCurrentClockApp();
 }
